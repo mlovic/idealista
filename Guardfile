f1892552a3787ef303398dca43b4a80a482dabd5
Marko Lovic <markolovic@hotmail.com>
2015-07-20 23:57:15 +0200

    Refactor RubifyKeys to metaprogram methods

--- Guardfile
+++ Guardfile
@@ -1,5 +1,6 @@
 guard :rspec, cmd: 'rspec' do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/idealista/(.+)\.rb$})     { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^lib/core_extensions/(.+)\.rb$})     { |m| "spec/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
 end
--- lib/core_extensions/rubify_keys.rb
+++ lib/core_extensions/rubify_keys.rb
@@ -1,28 +1,21 @@
 module CoreExtensions
   module RubifyKeys
 
-    # TODO metaprogram both definitions out of one to DRY up
+    # Creates methods #rubify_keys! and #unrubify_keys!
+    {"rubify" => "snake_case", "unrubify" => "camel_case"}.each do |name, kase|
+      method_name = "#{name}_keys!"
+      define_method(method_name) do 
 
-    def rubify_keys! # TODO change to rubyify?
-      keys.each{|k|
-        v = delete(k)
-        new_key = convert_to_snake_case(k.to_s)
-        self[new_key] = v
-        v.rubify_keys! if v.is_a?(Hash)
-        v.each{|p| p.rubify_keys! if p.is_a?(Hash)} if v.is_a?(Array)
-      }
-      self
-    end
+        keys.each do |key|
+          val = delete(key)
+          new_key = self.send("convert_to_#{kase}", key.to_s)
+          self[new_key] = val
+          val.send(method_name) if val.is_a?(Hash)
+          val.each { |p| p.send(method_name) if p.is_a?(Hash) } if val.is_a?(Array)
+        end
+        self
 
-    def unrubify_keys!
-      keys.each{|k|
-        v = delete(k)
-        new_key = convert_to_camel_case(k.to_s)
-        self[new_key] = v
-        v.unrubify_keys! if v.is_a?(Hash)
-        v.each{|p| p.unrubify_keys! if p.is_a?(Hash)} if v.is_a?(Array)
-      }
-      self
+      end
     end
 
     private_class_method
